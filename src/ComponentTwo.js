7f09594b39aacf151d5dd195da1ce4dad3a2df20
Venura <hvenura@gmail.com>
2022-06-28 21:19:45 +1000

    base react applicaiton first commit

---
+++ src/ComponentTwo.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React  from 'react';
+
+
+class ComponentTwo extends React.Component {
+
+  render() {
+      return <div>Hello World</div>;
+    }
+
+
+}
+
+export default ComponentTwo;
